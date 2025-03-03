34307802d659efffa2a492b0b44d3370c335d6e0
110011100101 <2285491147@qq.com>
2025-03-03 18:13:08 +0800

    Add an Earth animation in plate config menu.

---
+++ harmonious-republic/Code/Class/PlateNameLable.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class PlateNameLable : Label
+{
+	public void ChangeText(string text)
+	{
+		if (text == "")
+		{
+			Text = "板块名称";
+		}
+		else
+		{
+			Text = text;
+		}
+	}
+}
